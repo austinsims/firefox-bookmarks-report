77e93113e5d768f7859a4e161ed5523121a4a584
Austin Sims <adsims2001@gmail.com>
2021-02-13 11:02:46 -0600

    change port

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,12 @@
 import spark.kotlin.*
 
+val PORT = 8080
+
 fun main() {
     val http: Http = ignite()
+    http.port(PORT)
     http.get("/") {
         "Hello, world"
     }
+    println("Running at http://localhost:$PORT")
 }
