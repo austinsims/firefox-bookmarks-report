7812d81da798a73b6af9ffb5cdd343ac79e2c64a
Austin Sims <adsims2001@gmail.com>
2021-02-14 09:53:37 -0600

    open a copy

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,9 +3,13 @@ import com.google.common.collect.ImmutableList
 import com.google.common.io.Resources
 import java.io.StringWriter
 import java.nio.charset.StandardCharsets
+import java.nio.file.Files;
+import java.nio.file.Paths
+import java.nio.file.StandardCopyOption
 import java.sql.Connection
 import java.sql.DriverManager
 import java.time.Instant
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 import java.util.Locale
@@ -13,7 +17,6 @@ import org.apache.commons.cli.DefaultParser
 import org.apache.commons.cli.Options
 import spark.kotlin.Http
 import spark.kotlin.ignite
-import java.time.ZoneId
 
 val PORT = 8080
 val FLAG_PROFILE_PATH = "firefox_profile_path"
@@ -37,10 +40,13 @@ fun run(args: Array<String>) {
         true /* hasArg */,
         "Path to Firefox profile directory")
     val commandLine = DefaultParser().parse(options, args)
-    val profilePath = commandLine.getOptionValue(FLAG_PROFILE_PATH)
+    val profilePath = Paths.get(commandLine.getOptionValue(FLAG_PROFILE_PATH)).resolve("places.sqlite")
 
     // Connect to database
-    val connection = DriverManager.getConnection("jdbc:sqlite:$profilePath/places.sqlite")
+    // Make a copy since the database will be locked if Firefox is still open.
+    val copy = Paths.get("/tmp/places.sqlite")
+    Files.copy(profilePath, copy, StandardCopyOption.REPLACE_EXISTING)
+    val connection = DriverManager.getConnection("jdbc:sqlite:$copy")
 
     // Configure HTTP
     val http: Http = ignite()
